77dfd83eeebcde2fd586b0b814210cd367f1e03d
hanmarookim <hanmaroo.kim@gmail.com>
2020-12-07 17:58:27 +0900

    단어변환_python

---
+++ Level3 - 단어 변환/programmers_단어변환.py
@@ -0,0 +1,21 @@
+from collections import deque
+def solution(begin, target, words):
+    answer = 0
+    if target not in words: return 0
+    words = [begin] + words
+    visit = 0
+    q = deque([(0, 0)])
+    while q:
+        num, tick = q.popleft()
+        if not visit & 1<<num:
+            if words[num] == target: return tick
+            visit = visit | 1<<num
+            for w in range(len(words)):
+                dif = 0
+                for l in range(len(words[num])):
+                    if words[num][l] != words[w][l]: dif += 1
+                    if dif > 1: break
+                if dif == 1:
+                    q.append((w, tick+1))
+
+print(solution('hit', 'cog', ['hot', 'dot', 'dog', 'lot', 'log', 'cog']))
