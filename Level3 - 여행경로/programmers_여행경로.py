9c92b7e217ff223e5634a3b53864875aedac014e
Maroo KH <hanmaroo.kim@gmail.com>
2020-12-13 23:42:10 +0900

    programmers 여행경로

---
+++ Level3 - 여행경로/programmers_여행경로.py
@@ -0,0 +1,17 @@
+def solution(tickets):
+    answer = []
+    stack = []
+    visit = []
+    stack.append(['ICN', tickets, ['ICN']])
+    while stack:
+        node = stack.pop()
+        if len(node[1]) == 0:
+            visit.append(node[2])
+        for t in range(len(node[1])):
+            if node[1][t][0] == node[0]:
+                ti = node[1][:t] + node[1][t+1:]
+                stack.append([node[1][t][1], ti, node[2]+[node[1][t][1]]])
+    visit = sorted(visit)
+    return visit
+
+print(solution([["ICN", "SFO"], ["ICN", "ATL"], ["SFO", "ATL"], ["ATL", "ICN"], ["ATL","SFO"]]))
